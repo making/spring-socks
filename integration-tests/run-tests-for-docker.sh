eef24ca47474fb5da39ef1d5f998b42eaf60319f
Toshiaki Maki <tmaki@pivotal.io>
2020-07-20 17:03:48 +0900

    Fix url

--- integration-tests/run-tests-for-docker.sh
+++ integration-tests/run-tests-for-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-export SOCK_FRONTENDURL=http://localhost:808
+export SOCK_FRONTENDURL=http://localhost:8080
 
 ./mvnw test -f ${SCRIPT_DIR}/pom.xml
