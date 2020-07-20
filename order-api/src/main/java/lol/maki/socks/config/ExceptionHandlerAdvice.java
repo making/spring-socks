1ee2370a0f214840278e9e9d99ecde164bfa6464
Toshiaki Maki <tmaki@pivotal.io>
2020-07-20 14:42:34 +0900

    Add ExceptionHandlerAdvice

---
+++ order-api/src/main/java/lol/maki/socks/config/ExceptionHandlerAdvice.java
@@ -0,0 +1,25 @@
+package lol.maki.socks.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Order(0)
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+	private final Logger log = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+
+	@ExceptionHandler(WebClientResponseException.class)
+	public ResponseEntity<?> handleWebClientResponseException(WebClientResponseException e) {
+		log.warn(e.getMessage());
+		return ResponseEntity.status(e.getStatusCode())
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(e.getResponseBodyAsString());
+	}
+}
