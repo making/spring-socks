30f86b83bcc19ad5f4f58d33e804b4bb40e46427
Toshiaki Maki <tmaki@pivotal.io>
2020-07-20 16:48:16 +0900

    Fix url

--- integration-tests/run-tests-for-docker.sh
+++ integration-tests/run-tests-for-docker.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -ex
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-GATEWAY_URL=http://localhost:8080
-export SOCK_CATALOGURL=${GATEWAY_URL}
-export SOCK_PAYMENTURL=${GATEWAY_URL}
-export SOCK_SHIPPINGURL=${GATEWAY_URL}
-export SOCK_ORDERURL=${GATEWAY_URL}
-export SOCK_CARTURL=${GATEWAY_URL}
+export SOCK_FRONTENDURL=http://localhost:808
 
 ./mvnw test -f ${SCRIPT_DIR}/pom.xml
