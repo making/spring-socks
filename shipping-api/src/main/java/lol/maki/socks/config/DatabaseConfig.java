8e757cbd51f2aa9c35cb143560acbcb7c88912c6
Toshiaki Maki <tmaki@pivotal.io>
2020-07-20 11:45:18 +0900

    Add DatabaseConfig

---
+++ shipping-api/src/main/java/lol/maki/socks/config/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package lol.maki.socks.config;
+
+import java.util.stream.Stream;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+/**
+ * https://deinum.biz/2020-06-30-Wait-for-database-startup/
+ */
+@Configuration
+public class DatabaseConfig {
+	@Bean
+	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+		return bf -> {
+			final String[] flyway = bf.getBeanNamesForType(Flyway.class);
+			Stream.of(flyway)
+					.map(bf::getBeanDefinition)
+					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
+		};
+	}
+
+	@Bean
+	public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+		final DatabaseStartupValidator dsv = new DatabaseStartupValidator();
+		dsv.setDataSource(dataSource);
+		dsv.setValidationQuery(DatabaseDriver.MYSQL.getValidationQuery());
+		return dsv;
+	}
+}
