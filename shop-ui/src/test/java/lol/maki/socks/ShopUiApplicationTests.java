d76e9c4d4cd06bcb59b91496d5e66c5d37d017ad
Toshiaki Maki <tmaki@pivotal.io>
2020-10-12 09:10:35 +0900

    Fix test

--- shop-ui/src/test/java/lol/maki/socks/ShopUiApplicationTests.java
+++ shop-ui/src/test/java/lol/maki/socks/ShopUiApplicationTests.java
@@ -1,9 +1,12 @@
 package lol.maki.socks;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.security.oauth2.client.provider.ui.issuer-uri=https://uaa.run.pcfone.io/oauth/token",
+		"spring.security.oauth2.client.provider.sock.issuer-uri=https://uaa.run.pcfone.io/oauth/token" })
 class ShopUiApplicationTests {
 
 	@Test
