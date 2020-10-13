11e8cc710176894e45fa85f8c94d47aa8ca92b70
Toshiaki Maki <tmaki@pivotal.io>
2020-10-14 00:24:25 +0900

    [WIP] Add k8s manifest gh-13

---
+++ k8s/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eo pipefail
+
+APP=$1
+
+if [ "${APP}" == "" ];then
+  echo "Usage: $0 <APP>"
+  exit 1
+fi
+
+shift
+
+set -x
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+kapp deploy -a ${APP} \
+  -f ${SCRIPT_DIR}/../${APP}/k8s \
+  -c $@
