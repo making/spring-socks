898785415ce140043dc230a40ffb9035af5cee5a
Toshiaki Maki <tmaki@pivotal.io>
2020-10-14 18:26:28 +0900

    Add Spring socks closes gh-2

---
+++ catalog-api/src/main/resources/db/migration/V3__add_spring_socks.sql
@@ -0,0 +1,9 @@
+INSERT INTO sock
+VALUES ("6acfab6b-7574-4082-b37a-5ec94966eaff", "Spring",
+        "Put a little spring in your step", 21.0, 30, "/images/spring_socks_1.jpg",
+        "/images/spring_socks_2.jpg");
+
+INSERT INTO sock_tag
+VALUES ("6acfab6b-7574-4082-b37a-5ec94966eaff", 2);
+INSERT INTO sock_tag
+VALUES ("6acfab6b-7574-4082-b37a-5ec94966eaff", 11);
