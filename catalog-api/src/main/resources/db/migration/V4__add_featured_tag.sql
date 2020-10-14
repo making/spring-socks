8e447bd8080e6686849816b88a0935dc667045a7
Toshiaki Maki <tmaki@pivotal.io>
2020-10-14 18:35:40 +0900

    Add featured tag

---
+++ catalog-api/src/main/resources/db/migration/V4__add_featured_tag.sql
@@ -0,0 +1,15 @@
+INSERT INTO tag (name)
+VALUES ("featured");
+
+INSERT INTO sock_tag
+VALUES ("6acfab6b-7574-4082-b37a-5ec94966eaff", 12);
+INSERT INTO sock_tag
+VALUES ("819e1fbf-8b7e-4f6d-811f-693534916a8b", 12);
+INSERT INTO sock_tag
+VALUES ("abc4f044-b040-410d-8ead-4de0446aec7e", 12);
+INSERT INTO sock_tag
+VALUES ("a0a4f044-b040-410d-8ead-4de0446aec7e", 12);
+INSERT INTO sock_tag
+VALUES ("510a0d7e-8e83-4193-b483-e27e09ddc34d", 12);
+INSERT INTO sock_tag
+VALUES ("3395a43e-2d88-40de-b95f-e00e1502085b", 12);
