22da90acad2cbbcd18afe715acd9f9a0a9ee1e93
Toshiaki Maki <tmaki@pivotal.io>
2020-12-16 19:03:50 +0900

    Use MockMvcWebTestClient instead of MockMvc

--- catalog-api/src/test/java/lol/maki/socks/catalog/web/CatalogControllerTest.java
+++ catalog-api/src/test/java/lol/maki/socks/catalog/web/CatalogControllerTest.java
@@ -9,20 +9,20 @@ import lol.maki.socks.catalog.ImmutableSock;
 import lol.maki.socks.catalog.Sock;
 import lol.maki.socks.catalog.SockMapper;
 import lol.maki.socks.catalog.Tag;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 
 import static com.atlassian.oai.validator.mockmvc.OpenApiValidationMatchers.openApi;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(properties = "spring.security.oauth2.resourceserver.jwt.issuer-uri=https://uaa.run.pcfone.io/oauth/token", controllers = CatalogController.class)
 class CatalogControllerTest {
@@ -31,6 +31,8 @@ class CatalogControllerTest {
 
 	@MockBean
 	SockMapper sockMapper;
+
+	WebTestClient webTestClient;
 
 	private final Sock sock1 = ImmutableSock.builder()
 			.sockId(UUID.fromString("57b8db2f-15fc-4164-bfaf-ad30b55ef7e8"))
@@ -52,55 +54,70 @@ class CatalogControllerTest {
 			.count(20)
 			.build();
 
+	@BeforeEach
+	void setup() throws Exception {
+		this.webTestClient = MockMvcWebTestClient.bindTo(this.mockMvc).build();
+	}
+
 	@Test
+	@WithMockUser(authorities = "SCOPE_catalog:read")
 	void getSock() throws Exception {
 		given(this.sockMapper.findOne(this.sock1.sockId())).willReturn(Optional.of(this.sock1));
-		this.mockMvc.perform(get("/catalogue/{sockId}", "57b8db2f-15fc-4164-bfaf-ad30b55ef7e8")
-				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_catalog:read"))))
-				.andExpect(status().isOk())
-				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"))
-				.andExpect(jsonPath("$.id").value("57b8db2f-15fc-4164-bfaf-ad30b55ef7e8"))
-				.andExpect(jsonPath("$.name").value("demo1"))
-				.andExpect(jsonPath("$.description").value("Demo1"))
-				.andExpect(jsonPath("$.count").value(10))
-				.andExpect(jsonPath("$.price").value(10.0))
-				.andExpect(jsonPath("$.imageUrl.length()").value(2))
-				.andExpect(jsonPath("$.imageUrl[0]").value("/demo1_1.jpg"))
-				.andExpect(jsonPath("$.imageUrl[1]").value("/demo1_2.jpg"))
-				.andExpect(jsonPath("$.tag.length()").value(2))
-				.andExpect(jsonPath("$.tag[0]").value("blue"))
-				.andExpect(jsonPath("$.tag[1]").value("red"));
+		final EntityExchangeResult<byte[]> result = this.webTestClient.get()
+				.uri("/catalogue/{sockId}", "57b8db2f-15fc-4164-bfaf-ad30b55ef7e8")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$.id").isEqualTo("57b8db2f-15fc-4164-bfaf-ad30b55ef7e8")
+				.jsonPath("$.name").isEqualTo("demo1")
+				.jsonPath("$.description").isEqualTo("Demo1")
+				.jsonPath("$.count").isEqualTo(10)
+				.jsonPath("$.price").isEqualTo(10.0)
+				.jsonPath("$.imageUrl.length()").isEqualTo(2)
+				.jsonPath("$.imageUrl[0]").isEqualTo("/demo1_1.jpg")
+				.jsonPath("$.imageUrl[1]").isEqualTo("/demo1_2.jpg")
+				.jsonPath("$.tag.length()").isEqualTo(2)
+				.jsonPath("$.tag[0]").isEqualTo("blue")
+				.jsonPath("$.tag[1]").isEqualTo("red")
+				.returnResult();
+		MockMvcWebTestClient.resultActionsFor(result)
+				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"));
 	}
 
 
 	@Test
+	@WithMockUser(authorities = "SCOPE_catalog:read")
 	void getSocks() throws Exception {
 		given(this.sockMapper.findSocks(List.of(Tag.valueOf("red")), "price", 1, 10)).willReturn(List.of(this.sock1, this.sock2));
-		this.mockMvc.perform(get("/catalogue").param("tags", "red")
-				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_catalog:read"))))
-				.andExpect(status().isOk())
-				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"))
-				.andExpect(jsonPath("$.length()").value(2))
-				.andExpect(jsonPath("$[0].id").value("57b8db2f-15fc-4164-bfaf-ad30b55ef7e8"))
-				.andExpect(jsonPath("$[0].name").value("demo1"))
-				.andExpect(jsonPath("$[0].description").value("Demo1"))
-				.andExpect(jsonPath("$[0].count").value(10))
-				.andExpect(jsonPath("$[0].price").value(10.0))
-				.andExpect(jsonPath("$[0].imageUrl.length()").value(2))
-				.andExpect(jsonPath("$[0].imageUrl[0]").value("/demo1_1.jpg"))
-				.andExpect(jsonPath("$[0].imageUrl[1]").value("/demo1_2.jpg"))
-				.andExpect(jsonPath("$[0].tag.length()").value(2))
-				.andExpect(jsonPath("$[0].tag[0]").value("blue"))
-				.andExpect(jsonPath("$[0].tag[1]").value("red"))
-				.andExpect(jsonPath("$[1].id").value("bb4f7c35-67f9-4f0b-90c2-69cde0964fbd"))
-				.andExpect(jsonPath("$[1].name").value("demo2"))
-				.andExpect(jsonPath("$[1].description").value("Demo2"))
-				.andExpect(jsonPath("$[1].count").value(20))
-				.andExpect(jsonPath("$[1].price").value(20.0))
-				.andExpect(jsonPath("$[1].imageUrl.length()").value(2))
-				.andExpect(jsonPath("$[1].imageUrl[0]").value("/demo2_1.jpg"))
-				.andExpect(jsonPath("$[1].imageUrl[1]").value("/demo2_2.jpg"))
-				.andExpect(jsonPath("$[1].tag.length()").value(1))
-				.andExpect(jsonPath("$[1].tag[0]").value("red"));
+		final EntityExchangeResult<byte[]> result = this.webTestClient.get()
+				.uri("/catalogue?tags=red")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(2)
+				.jsonPath("$[0].id").isEqualTo("57b8db2f-15fc-4164-bfaf-ad30b55ef7e8")
+				.jsonPath("$[0].name").isEqualTo("demo1")
+				.jsonPath("$[0].description").isEqualTo("Demo1")
+				.jsonPath("$[0].count").isEqualTo(10)
+				.jsonPath("$[0].price").isEqualTo(10.0)
+				.jsonPath("$[0].imageUrl.length()").isEqualTo(2)
+				.jsonPath("$[0].imageUrl[0]").isEqualTo("/demo1_1.jpg")
+				.jsonPath("$[0].imageUrl[1]").isEqualTo("/demo1_2.jpg")
+				.jsonPath("$[0].tag.length()").isEqualTo(2)
+				.jsonPath("$[0].tag[0]").isEqualTo("blue")
+				.jsonPath("$[0].tag[1]").isEqualTo("red")
+				.jsonPath("$[1].id").isEqualTo("bb4f7c35-67f9-4f0b-90c2-69cde0964fbd")
+				.jsonPath("$[1].name").isEqualTo("demo2")
+				.jsonPath("$[1].description").isEqualTo("Demo2")
+				.jsonPath("$[1].count").isEqualTo(20)
+				.jsonPath("$[1].price").isEqualTo(20.0)
+				.jsonPath("$[1].imageUrl.length()").isEqualTo(2)
+				.jsonPath("$[1].imageUrl[0]").isEqualTo("/demo2_1.jpg")
+				.jsonPath("$[1].imageUrl[1]").isEqualTo("/demo2_2.jpg")
+				.jsonPath("$[1].tag.length()").isEqualTo(1)
+				.jsonPath("$[1].tag[0]").isEqualTo("red")
+				.returnResult();
+		MockMvcWebTestClient.resultActionsFor(result)
+				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"));
 	}
 }
--- catalog-api/src/test/java/lol/maki/socks/catalog/web/TagControllerTest.java
+++ catalog-api/src/test/java/lol/maki/socks/catalog/web/TagControllerTest.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 import lol.maki.socks.catalog.Tag;
 import lol.maki.socks.catalog.TagMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 
 import static com.atlassian.oai.validator.mockmvc.OpenApiValidationMatchers.openApi;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(properties = "spring.security.oauth2.resourceserver.jwt.issuer-uri=https://uaa.run.pcfone.io/oauth/token", controllers = TagController.class)
 class TagControllerTest {
@@ -33,17 +33,28 @@ class TagControllerTest {
 
 	private final Tag tag3 = Tag.valueOf("green");
 
+	WebTestClient webTestClient;
+
+	@BeforeEach
+	void setup() throws Exception {
+		this.webTestClient = MockMvcWebTestClient.bindTo(this.mockMvc).build();
+	}
 
 	@Test
+	@WithMockUser(authorities = "SCOPE_catalog:read")
 	void getTags() throws Exception {
 		given(this.tagMapper.findAll()).willReturn(List.of(this.tag1, this.tag2, this.tag3));
-		this.mockMvc.perform(get("/tags")
-				.with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_catalog:read"))))
-				.andExpect(status().isOk())
-				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"))
-				.andExpect(jsonPath("$.tags.length()").value(3))
-				.andExpect(jsonPath("$.tags[0]").value("red"))
-				.andExpect(jsonPath("$.tags[1]").value("blue"))
-				.andExpect(jsonPath("$.tags[2]").value("green"));
+		final EntityExchangeResult<byte[]> result = this.webTestClient.get()
+				.uri("/tags")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$.tags.length()").isEqualTo(3)
+				.jsonPath("$.tags[0]").isEqualTo("red")
+				.jsonPath("$.tags[1]").isEqualTo("blue")
+				.jsonPath("$.tags[2]").isEqualTo("green")
+				.returnResult();
+		MockMvcWebTestClient.resultActionsFor(result)
+				.andExpect(openApi().isValid("META-INF/resources/openapi/doc.yml"));
 	}
 }
