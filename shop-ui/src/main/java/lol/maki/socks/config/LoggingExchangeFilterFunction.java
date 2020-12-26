75d65d36d15590e77553e0622b69b6aad629932f
Toshiaki Maki <tmaki@pivotal.io>
2020-12-26 16:54:04 +0900

    Refactor

--- shop-ui/src/main/java/lol/maki/socks/config/LoggingExchangeFilterFunction.java
+++ shop-ui/src/main/java/lol/maki/socks/config/LoggingExchangeFilterFunction.java
@@ -13,43 +13,39 @@ import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 public enum LoggingExchangeFilterFunction implements ExchangeFilterFunction {
-	SINGLETON {
-		@Override
-		public Mono<ClientResponse> filter(ClientRequest clientRequest, ExchangeFunction exchangeFunction) {
-			final AtomicLong begin = new AtomicLong();
-			if (log().isDebugEnabled()) {
-				log().info("--> {} {}", clientRequest.method(), clientRequest.url());
-				clientRequest.headers().forEach((k, v) -> {
-					log().info("{}: {}", k, String.join(",", v));
-				});
-				log().info("--> END {}", clientRequest.method());
-				begin.set(System.currentTimeMillis());
-			}
-			return exchangeFunction.exchange(clientRequest)
-					.doOnNext(clientResponse -> {
-						if (log().isDebugEnabled()) {
-							final long elapsed = System.currentTimeMillis() - begin.get();
-							log().info("<-- {} {} ({}ms)", clientResponse.statusCode(), clientRequest.url(), elapsed);
-							clientResponse.headers().asHttpHeaders().forEach((k, v) -> {
-								log().info("{}: {}", k, String.join(",", v));
-							});
-							log().info("<-- END HTTP");
-						}
-					})
-					.doOnError(e -> {
-						if (log().isDebugEnabled()) {
-							final long elapsed = System.currentTimeMillis() - begin.get();
-							final Object status = (e instanceof WebClientResponseException) ? ((WebClientResponseException) e).getStatusCode() : "000";
-							log().info("<-- {} {} ({}ms)", status, clientRequest.url(), elapsed);
-						}
-						log().info("<-- FAILED HTTP");
-					});
-		}
-	};
+	SINGLETON;
 
 	private final Logger log = LoggerFactory.getLogger(LoggingExchangeFilterFunction.class);
 
-	final Logger log() {
-		return log;
+	@Override
+	public Mono<ClientResponse> filter(ClientRequest clientRequest, ExchangeFunction exchangeFunction) {
+		final AtomicLong begin = new AtomicLong();
+		if (log.isDebugEnabled()) {
+			log.info("--> {} {}", clientRequest.method(), clientRequest.url());
+			clientRequest.headers().forEach((k, v) -> {
+				log.info("{}: {}", k, String.join(",", v));
+			});
+			log.info("--> END {}", clientRequest.method());
+			begin.set(System.currentTimeMillis());
+		}
+		return exchangeFunction.exchange(clientRequest)
+				.doOnNext(clientResponse -> {
+					if (log.isDebugEnabled()) {
+						final long elapsed = System.currentTimeMillis() - begin.get();
+						log.info("<-- {} {} ({}ms)", clientResponse.statusCode(), clientRequest.url(), elapsed);
+						clientResponse.headers().asHttpHeaders().forEach((k, v) -> {
+							log.info("{}: {}", k, String.join(",", v));
+						});
+						log.info("<-- END HTTP");
+					}
+				})
+				.doOnError(e -> {
+					if (log.isDebugEnabled()) {
+						final long elapsed = System.currentTimeMillis() - begin.get();
+						final Object status = (e instanceof WebClientResponseException) ? ((WebClientResponseException) e).getStatusCode() : "000";
+						log.info("<-- {} {} ({}ms)", status, clientRequest.url(), elapsed);
+					}
+					log.info("<-- FAILED HTTP");
+				});
 	}
 }
