dbfd2fa0189b093db6ea92381ef66c32d3e8f003
Toshiaki Maki <tmaki@pivotal.io>
2020-12-28 17:01:24 +0900

    permit static resources

--- shop-ui/src/main/java/lol/maki/socks/config/SecurityConfig.java
+++ shop-ui/src/main/java/lol/maki/socks/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -51,6 +52,7 @@ public class SecurityConfig {
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		return http
 				.authorizeExchange(exchanges -> exchanges
+						.matchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
 						.matchers(EndpointRequest.to("health", "info", "prometheus")).permitAll()
 						.anyExchange().permitAll()
 				)
